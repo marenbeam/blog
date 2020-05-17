a8ec7d2abcf2000c8a31701381476197ce5a0ebd
Maren Beam <marenbeam@mailbox.org>
2020-05-17 08:50:04 -0400

    update blog

--- build.sh
+++ build.sh
@@ -7,11 +7,14 @@ file=$1
 # filename="${filename%.*}"
 
 rm footer.html
-echo "<footer>" >> footer.html
-echo "last edited on $(date)" >> footer.html
+echo '<footer>' >> footer.html
+echo "Last edited on $(date)<br>" >> footer.html
+echo '<br>' >> footer.html
+echo 'Content licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA</a><br>' >> footer.html
+echo 'Source code licensed under <a href="https://opensource.org/licenses/MIT">MIT</a> unless otherwise noted<br>' >> footer.html
+echo '<br>' >> footer.html
 echo '<script async defer src="https://www.recurse-scout.com/loader.js?t=9ba46999660e9e2af4e35e19d38328d1"></script>' >> footer.html
-echo '<br><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a>' >> footer.html
-echo "<footer>" >> footer.html
+echo '<footer>' >> footer.html
 
 pandoc $file --from markdown --to html --css style.css --output "${file%.*}.html" --highlight-style pygments --filter ./rainbow.py --include-after-body footer.html --include-before-body header.html
 firefox "${file%.*}.html"
